4af30cd3adbd1133b3c1f192890e101ea3268304
James Sweeny <james.sweeny@puppetlabs.com>
2016-01-12 13:01:21 -0500

    remove array validation from removed paramter

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,6 @@ define pe_shared_cert (
 ) {
 
   validate_string($certname)
-  validate_array($dns_alt_names)
 
   if file("/etc/puppetlabs/puppet/ssl/private_keys/${certname}.pem",'/dev/null') == '' {
     fail("Pe_shared_cert[${certname}] requires that a private key is pre-generated. Please ensure that Pe_shared_cert::Ca[${certname}] is applied to the CA, and that the CA is the Puppet Master for this agent run.")
